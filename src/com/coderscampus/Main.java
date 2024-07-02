6346eda0dde5cb73c5505e468e81e8d197525c58
Jean Arnaud Tanoe <jatan60@gmail.com>
2024-07-01 22:33:33 -0500

    Updated comments in Main.java file

--- src/com/coderscampus/Main.java
+++ src/com/coderscampus/Main.java
@@ -14,22 +14,22 @@ public class Main {
 		// Declares the courses names variable
 		String[] courses =  { "APMTH", "STAT", "COMPSCI" };
 
-		// Parse the Master List File and get the data
+		/** Step 1: Get data from the Master List File */
 		StudentService masterService = new StudentService("student-master-list.csv");
 		masterService.setStudents();
 		Student[] masterList = masterService.getStudents();
 		
-		/** Step 1: Parse the Master List File  */		
+		/** Step 2: Parse data and separate students by course  */		
 		Student[] course1 = masterService.getStudentsByCourse(masterList, courses[0]);
 		Student[] course2 = masterService.getStudentsByCourse(masterList, courses[1]);
 		Student[] course3 = masterService.getStudentsByCourse(masterList, courses[2]);
 		
-		/** Step 2: Sort the students by grade in descending order */
+		/** Step 3: Sort the students by grade in descending order */
 		Arrays.sort(course1);
 		Arrays.sort(course2);
 		Arrays.sort(course3);
 		
-		/** Step2: Separate the Master List File data into 3 separate CSV files */
+		/** Step 4: Separate the Master List File data into 3 separate CSV files */
 		FileService writer1 = new FileService(course1, "course1.csv");
 		writer1.write();
 		FileService writer2 = new FileService(course2, "course2.csv");
